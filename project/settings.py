843af7be0bb54ac69607bffcd97777c3449c3bff
Supermaneman <emanuel.rabe18@gmail.com>
2022-03-25 04:34:25 +0800

    jh

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ms%h@w1rth)fe7tor^yrve@ucnizx7fvs%x%@z$xpi*mbn!#a4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rabe-kopikrumblereservation.azurewebsites.net']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleWare',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,12 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+#MEDIA_URL = '/images/'
 
-STATICFILES_DIRS=(
-    os.path.join(BASE_DIR,'static'),
-)
+#STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFileStorage'
 
+#STATICFILES_DIRS=(
+#    os.path.join(BASE_DIR,'static'),
+#)
+STATIC_ROOT= BASE_DIR/'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
